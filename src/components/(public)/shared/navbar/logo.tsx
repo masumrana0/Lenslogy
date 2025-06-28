66ea4e299ad76514243add193e7b377fbcbd234d
Masum Rana <masumrana446267@gmail.com>
2025-06-28 18:48:02 +0600

    change logo for test

--- src/components/(public)/shared/navbar/logo.tsx
+++ src/components/(public)/shared/navbar/logo.tsx
@@ -6,8 +6,8 @@ const Logo = ({ className }: { className?: string }) => {
     <Link href="/" className={`shrink-0 ${className}`}>
       <div className="flex items-center">
         <span className="font-bold text-2xl">
-          <span className="text-black dark:text-white">Masum</span>
-          <span className="text-red-500 dark:text-red-400">Logy</span>
+          <span className="text-black dark:text-white">Masum </span>
+          <span className="text-red-500 dark:text-red-400">Rana</span>
         </span>
       </div>
     </Link>
